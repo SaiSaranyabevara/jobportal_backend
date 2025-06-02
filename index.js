bf66cb6bb0f9008d9e5e85f0e6de2ba0d407e3f0
Bevara Saranya <127865395+SaiSaranyabevara@users.noreply.github.com>
2025-06-02 13:34:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions=({
-    origin:"http://localhost:5173",
+   origin:["http://localhost:5173",
+  "https://jobportal-frontend-green.vercel.app"
+    ],
     credentials:true
 });
 app.use(cors(corsOptions));
